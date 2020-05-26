119c7f63a84a564868e0b3f456946a8ba89d36b6
Andrew Quinn <andrew@aquinn.org>
2020-05-26 03:48:03 -0700

    Separated rendering from fractal calculation

--- gofrac.go
+++ gofrac.go
@@ -2,48 +2,16 @@ package gofrac
 
 import (
 	"errors"
-	"github.com/lucasb-eyer/go-colorful"
-	"image/color"
 	"math/cmplx"
 )
 
 //var debug = log.New(os.Stdout, "DEBUG: ", log.LstdFlags)
 
-const maxIterations = 50
+const maxIterations = 35
 
-// colorPalette stores a numerically indexed lookup table of colors
-type colorPalette [maxIterations]color.Color
-
-// newSpectralPalette creates a spectral color map, but with the final element colored black
-func newSpectralPalette(sweep float64, offset float64) (p colorPalette) {
-	for i := 0; i < maxIterations-1; i++ {
-		t := float64(i) / float64(maxIterations-1)
-		p[i] = colorful.Hsv(sweep*t+offset, 1.0, 1.0).Clamped()
-	}
-	p[len(p)-1] = color.Black
-	return p
-}
-
-// newRainbowPalette creates the familiar (and !!not colorblind-friendly!!) rainbow color map, but with the final element colored black
-func newRainbowPalette() colorPalette {
-	return newSpectralPalette(360.0, 0.0)
-}
-
-// bitmap stores a 2D field of color.Color that can be used to generate images
-type bitmap [][]color.Color
-
-// newBitmap initializes a bitmap
-func newBitmap(r int, c int) *bitmap {
-	b := make(bitmap, r)
-	for r := range b {
-		b[r] = make([]color.Color, c)
-	}
-	return &b
-}
-
-// Mandelbrot generates the Mandelbrot set and encodes the escape time as an element of a color palette. The parameters
+// Mandelbrot generates the Mandelbrot set and saves the results to a 2D slice of Results. The parameters
 // w and h are the number of samples to be taken along the horizontal and vertical axes of the domain, respectively.
-func Mandelbrot(w int, h int) (*bitmap, error) {
+func Mandelbrot(w int, h int) (*Results, error) {
 	if w == 0 || h == 0 {
 		return nil, errors.New("gofrac: w and h must both be greater than zero")
 	}
@@ -62,7 +30,7 @@ func Mandelbrot(w int, h int) (*bitmap, error) {
 			var c = complex(x, y)
 
 			count := 0
-			for mod := cmplx.Abs(z); mod <= 4.0; mod, count = cmplx.Abs(z), count+1 {
+			for mod := cmplx.Abs(z); mod <= 16.0; mod, count = cmplx.Abs(z), count+1 {
 				z = z*z + c
 
 				if count == maxIterations-1 {
@@ -73,14 +41,5 @@ func Mandelbrot(w int, h int) (*bitmap, error) {
 		}
 	}
 
-	// this will go away shortly
-	bitmap := newBitmap(h, w)
-	palette := newRainbowPalette()
-	for row := 0; row < domain.RowCount(); row++ {
-		for col := 0; col < domain.ColCount(row); col++ {
-			_, _, iterations := results.At(row, col)
-			(*bitmap)[row][col] = palette[iterations]
-		}
-	}
-	return bitmap, nil
+	return results, nil
 }
---
+++ render.go
@@ -0,0 +1,52 @@
+package gofrac
+
+import (
+	"github.com/lucasb-eyer/go-colorful"
+	"image/color"
+	"math"
+	"math/cmplx"
+)
+
+// colorPalette stores a numerically indexed lookup table of colors
+type colorPalette [maxIterations]color.Color
+
+// newSpectralPalette creates a spectral color map, but with the final element colored black
+func newSpectralPalette(sweep float64, offset float64) (p colorPalette) {
+	for i := 0; i < maxIterations-1; i++ {
+		t := float64(i) / float64(maxIterations-1)
+		p[i] = colorful.Hsv(sweep*t+offset, 1.0, 1.0).Clamped()
+	}
+	p[len(p)-1] = color.Black
+	return p
+}
+
+// newRainbowPalette creates the familiar (and !!not colorblind-friendly!!) rainbow color map, but with the final element colored black
+func newRainbowPalette() colorPalette {
+	return newSpectralPalette(210.0, 135.0)
+}
+
+// bitmap stores a 2D field of color.Color that can be used to generate images
+type bitmap [][]color.Color
+
+// newBitmap initializes a bitmap
+func newBitmap(r int, c int) bitmap {
+	b := make(bitmap, r)
+	for r := range b {
+		b[r] = make([]color.Color, c)
+	}
+	return b
+}
+
+func RenderEscapeTime(results ResultsReader) bitmap {
+	rows, cols := results.Dimensions()
+	bitmap := newBitmap(rows, cols)
+	palette := newRainbowPalette()
+	for row := 0; row < rows; row++ {
+		for col := 0; col < cols; col++ {
+			_, _, iterations := results.At(row, col)
+			bitmap[row][col] = palette[iterations]
+		}
+	}
+
+	return bitmap
+}
